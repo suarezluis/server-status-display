6b5f2a0c53aa437cc664a0f3cfe24dad4361ec9b
Luis Suarez <suarezluis@gmail.com>
2023-09-27 00:04:46 -0500

    add loading

--- python/status.py
+++ python/status.py
@@ -9,7 +9,7 @@ import subprocess
 import spidev as SPI
 from datetime import datetime
 
-sys.path.append(".")
+sys.path.append("..")
 from lib import LCD_1inch9
 from PIL import Image, ImageDraw, ImageFont
 
@@ -46,16 +46,29 @@ disp.Init()
 disp.clear()
 is_first_loop = True
 
+logging.info("draw loading")
+
+image0 = Image.new("RGB", (disp.width, disp.height), "BLACK")
+image1 = Image.open(__path__ + "/Image/luis.png")
+draw = ImageDraw.Draw(image0)
+image0.paste(image1)
+# image = Image.open(ImagePath[i])
+# image = image.rotate(0)
+disp.ShowImage(image0)
+time.sleep(5)
+
 logging.info("draw text")
+
 while True:
     try:
         now = datetime.now()
+        uptime = (
+            subprocess.check_output("uptime", shell=True).decode("utf-8").split(",")[0]
+        )
         current_minutes_seconds = now.strftime("%M:%S")
-
         # Create blank image for drawing.
         image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
         draw = ImageDraw.Draw(image1)
-
         cmd = "hostname"
         Hostname = subprocess.check_output(cmd, shell=True).decode("utf-8")
         cmd = "hostname -I | cut -d' ' -f1"
@@ -64,102 +77,115 @@ while True:
         if current_minutes_seconds == "00:00" or is_first_loop:
             cmd = "curl -s icanhazip.com"
             IP_EXT = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-        cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        cmd = "top -bn1 | grep load | awk '{printf \"Load: %.2f\", $(NF-2)}'"
         CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
         cmd = "free -m | awk 'NR==2{printf \"%s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
         MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
         cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
         Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
+        cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"T: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
         Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
         cmd = "sudo cat /etc/wpa_supplicant/wpa_supplicant.conf | grep ssid="
         SSID = subprocess.check_output(cmd, shell=True).decode("utf-8").split('"')
-
         lines = [
+            {
+                "label": "",
+                "labelColor": "GREEN",
+                "value": "Hostname: " + Hostname,
+                "valueColor": "WHITE",
+                "labelFont": Font20,
+                "valueFont": Font18,
+                "innerMargin": 25,
+                "marginBottom": 20,
+            },
+            {
+                "label": "",
+                "labelColor": "GREEN",
+                "value": "SSID: " + SSID[1],
+                "valueColor": "WHITE",
+                "labelFont": Font20,
+                "valueFont": Font18,
+                "innerMargin": 25,
+                "marginBottom": 20,
+            },
+            {
+                "label": "Up Time",
+                "labelColor": "GREEN",
+                "value": uptime.split("up ")[1],
+                "valueColor": "LIGHTGRAY",
+                "labelFont": Font20,
+                "valueFont": Font18,
+                "innerMargin": 25,
+                "marginBottom": 20,
+            },
             {
                 "label": "Internal IP",
                 "labelColor": "GREEN",
                 "value": IP,
-                "valueColor": "YELLOW",
-                "font": Font20,
-            },
-            {
-                "label": "Hostname: " + Hostname,
-                "labelColor": "LIGHTGRAY",
-                "value": "",
-                "valueColor": "",
-                "font": Font24,
+                "valueColor": "LIGHTGRAY",
+                "labelFont": Font20,
+                "valueFont": Font18,
+                "innerMargin": 25,
+                "marginBottom": 20,
             },
             {
                 "label": "External IP",
                 "labelColor": "GREEN",
                 "value": IP_EXT,
-                "valueColor": "YELLOW",
-                "font": Font22,
+                "valueColor": "LIGHTGRAY",
+                "labelFont": Font20,
+                "valueFont": Font18,
+                "innerMargin": 25,
+                "marginBottom": 20,
             },
             {
-                "label": Temp,
-                "labelColor": "LIGHTBLUE",
-                "value": "",
-                "valueColor": "YELLOW",
-                "font": Font20,
-            },
-            {
-                "label": CPU,
-                "labelColor": "ORANGE",
-                "value": "",
-                "valueColor": "YELLOW",
-                "font": Font20,
-            },
-            {
-                "label": "Memory",
+                "label": "CPU",
                 "labelColor": "GREEN",
-                "value": MemUsage,
-                "valueColor": "YELLOW",
-                "font": Font16,
+                "value": Temp + " " + CPU,
+                "valueColor": "LIGHTGRAY",
+                "labelFont": Font20,
+                "valueFont": Font18,
+                "innerMargin": 25,
+                "marginBottom": 20,
             },
             {
-                "label": Disk,
+                "label": "Memory / Storage",
                 "labelColor": "GREEN",
-                "value": "",
-                "valueColor": "YELLOW",
-                "font": Font19,
-            },
-            {
-                "label": "SSID: " + SSID[1],
-                "labelColor": "BLUE",
-                "value": "",
-                "valueColor": "YELLOW",
-                "font": Font22,
+                "value": MemUsage + "\n" + Disk,
+                "valueColor": "LIGHTGRAY",
+                "labelFont": Font20,
+                "valueFont": Font18,
+                "innerMargin": 25,
+                "marginBottom": 45,
             },
         ]
         lineY = 5
-        draw.text((95, lineY), now.strftime("%H:%M:%S"), fill="RED", font=Font22)
+        draw.text((90, lineY), now.strftime("%H:%M:%S"), fill="ORANGE", font=Font20)
         lineY = 30
         for line in lines:
-            draw.text(
-                (5, lineY), line["label"], fill=line["labelColor"], font=line["font"]
-            )
-            lineY = lineY + 25
+            if line["label"]:
+                draw.text(
+                    (5, lineY),
+                    line["label"],
+                    fill=line["labelColor"],
+                    font=line["labelFont"],
+                )
+                lineY = lineY + line["innerMargin"]
             if line["value"]:
                 draw.text(
                     (5, lineY),
                     line["value"],
                     fill=line["valueColor"],
-                    font=line["font"],
+                    font=line["valueFont"],
                 )
-                lineY = lineY + 25
-
+                lineY = lineY + line["marginBottom"]
         image1 = image1.rotate(0)
         disp.ShowImage(image1)
         is_first_loop = False
         time.sleep(1)
-
     except IOError as e:
         logging.info(e)
     except KeyboardInterrupt:
         disp.module_exit()
-
         logging.info("quit:")
         exit()
